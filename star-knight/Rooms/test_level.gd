08a861a68d700b304d08e75dce7c88e5dbba8866
awhipple906 <alexander.cary.whipple@gmail.com>
2025-11-10 15:10:31 -0500

    GrobCat
    Using GrobCat as my Guinea Pig for getting an enemy formula figured out. Something we can use to quickly add more once all the other parts are made. Should have that figured by end middle of week.

--- star-knight/Rooms/test_level.gd
+++ star-knight/Rooms/test_level.gd
@@ -1,7 +1,8 @@
 extends Node
 
 
-@onready var player = $player
+@onready var player = $test_blob
 
+#Called logic in every room so the enemies can know where the player is.
 func _physics_process(delta: float) -> void:
-	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
+	get_tree().call_group("Enemies", "update_target_location", player.global_transform.origin)
