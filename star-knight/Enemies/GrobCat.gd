04418b03f73aa0b50b6342acd9fa640963c321e1
awhipple906 <alexander.cary.whipple@gmail.com>
2025-11-11 15:05:00 -0500

    Nearly Completed Enemy Animation Script
    Wrote a generic animation script we can use for every enemy going forward. Needs tweaking, pushing what I have for now.

--- star-knight/Enemies/GrobCat.gd
+++ star-knight/Enemies/GrobCat.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 @export var max_health:= 10.0
 @export var max_speed := 5.0
 @export var attack_range := 2.0
+@export var animation : AnimatedSprite3D
+var animation_player
+var player
 
 var stunned := false
 var health
@@ -15,7 +18,10 @@ var current_speed := 0.0
 
 func _ready() -> void:
 	randomize()
-	var player = get_node(".../test_blob.tscn")
+	player = %test_blob
+	print(player)
+	animation_player = EnemyMovementAnimations.new()
+	animation_player.set_animation(animation)
 
 #This is the Grobcat pathing to the player
 func _physics_process(delta: float) -> void:
@@ -24,28 +30,21 @@ func _physics_process(delta: float) -> void:
 	var new_velocity = (next_location - current_position).normalized() * max_speed
 	
 	velocity = new_velocity
+	animation_player.play_movement_animations(velocity)
+	if (target_in_range()):
+		animation_player.play_attack_animations()
+		print("Attacking!")
+	else:
+		print("Not Attacking!")
 
-	if velocity.x < 0:
-		$GrobCatSprite3d.play(last_animation)
-	if velocity.z < 0:
-		$GrobCatSprite3d.flip_h = (velocity.z < 0)
-		print("FLIPPING ANIMATION>>>>>>")
-		$GrobCatSprite3d.play("move")
-		last_animation = "move"
-	if velocity.x > 0:
-		$GrobCatSprite3d.play(last_animation)
-	if velocity.z > 0:
-		$GrobCatSprite3d.flip_h = (velocity.z > 0)
-		$GrobCatSprite3d.play("move")
-		last_animation = "move"
 	move_and_slide()
-	
+
+		
 func update_target_location(target_location):
 	nav_agent.target_position = target_location
 	
-func target_in_range(player):
+func target_in_range():
+	if !player:
+		return
 	return global_position.distance_to(player.global_position) < attack_range
-	isAttacking = true
-	$GrobCatSprite3d.play("bite")
-	last_animation = "bite"
 	
