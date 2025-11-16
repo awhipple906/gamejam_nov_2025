3f711affaffd47c22fdd5d01874b59a9dd3f4627
awhipple906 <alexander.cary.whipple@gmail.com>
2025-11-16 13:29:51 -0500

    Updated MeleeAttack Logic to be more universal
    Also removed a few unnecessary cluttering files and Refactored test_blob to be named Player

--- star-knight/Enemies/GrobCat/Scripts/AttackStats.gd
+++ star-knight/Enemies/GrobCat/Scripts/AttackStats.gd
@@ -7,22 +7,41 @@ class_name AttackStats
 @export var max_peirce:= 1
 @export var knockback_force := 1.0
 @export var attack_cooldown := 5.0
+@export var attack_range := 1.0
+@export var attack_hitbox : CollisionShape3D
 var isOnCoolDown = false
-var attack_hit_box : CollisionShape3D
+
+func _check_can_attack(attack_stats : AttackStats):
+	if(attack_stats.has_overlapping_bodies() and !target_out_of_range(%Player) and !attack_stats.isOnCoolDown):
+		return true
+	else:
+		return false
 
 
 func _do_attack(body: Node3D) -> void:
+	var hitbox : HitboxComponent
+	var attack = Attack.new()
+	attack.attack_damage = damage
+	attack.knockback_force = knockback_force
 	if body is Player and !isOnCoolDown:
-		print("HITBOX ENTERED")
-		var hitbox : HitboxComponent = body.playerHitboxComponent
-		
-		var attack = Attack.new()
-		attack.attack_damage = damage
-		attack.knockback_force = knockback_force
+		print("PLAYER HITBOX ENTERED")
+		hitbox = body.playerHitboxComponent
 		print("Dealing: " + str(damage) + " to Player")
 		hitbox.damage(attack)
 		isOnCoolDown = true
+	elif body.get_groups().has("Enemies") and !isOnCoolDown:
+		print("ENEMY HITBOX ENTERED")
+		hitbox = body.EnemyHitboxComponent
+		print("Dealing: " + str(damage) + " to Enemy")
+		hitbox.damage(attack)
+		isOnCoolDown = true
+
 	await cooling_down()
+
+func target_out_of_range(target):
+	if !target:
+		return
+	return global_position.distance_to(target.global_position) > attack_range
 
 func cooling_down():
 	await get_tree().create_timer(attack_cooldown).timeout
---
+++ star-knight/Rooms/Traps/Spikes/spikes.gd
@@ -0,0 +1,16 @@
+extends Node3D
+class_name MeleeTrap
+@export var attack_stats : AttackStats
+@onready var player = %Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (attack_stats._check_can_attack(attack_stats)):
+		print("Spike sees you")
+		attack_stats._do_attack(attack_stats.get_overlapping_bodies()[0])
+		
