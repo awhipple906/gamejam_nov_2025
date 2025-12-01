5a74e0a8eea2bbf8b4aeece50563add5d4e9f465
awhipple906 <alexander.cary.whipple@gmail.com>
2025-12-01 12:31:14 -0500

    Basic Fixes
    Fixed Melee attack, added knockback where the distance is determined randomly (ciritcal hitting). Fixed Tutorial rooms so they function linearly. Tweaked jump animation so it does not play endlessly.

--- star-knight/Rooms/test_level.gd
+++ star-knight/Rooms/test_level.gd
@@ -1,33 +1,4 @@
-extends Node3D
-
-@onready var player = %Player
-@onready var mesh = %NavigationRegion3D
-
-
-func _ready():
-	print(current_scene)
-	##Tutorial Rooms will go in sequence
-	if current_scene in roomConstants.TutorialRooms:
-		next_scene_index = roomConstants.TutorialRooms.find(current_scene,0) + 1
-		if (next_scene_index >= roomConstants.TutorialRooms.size()):
-			next_scene = "HubRoom"
-		else:
-			next_scene = roomConstants.TutorialRooms[next_scene_index]
-		print(next_scene)
-	##Any other room will randomly proceed current room
-	elif  current_scene in roomConstants.Rooms:
-		next_scene_index = randi_range(0, roomConstants.Rooms.size() - 1)
-		next_scene = roomConstants.Rooms[next_scene_index]
-
-func run_dialogue(dialogue_string):
-	Dialogic.start(dialogue_string)
-
-#Called logic in every room so the enemies can know where the player is.
-func _physics_process(delta: float) -> void:
-	if(!player):
-		print("NO PLAYER DETECTED")
-		return
-	get_tree().call_group("Enemies", "update_target_location", player.global_transform.origin)
+extends Node
 
 var roomConstants = RoomConstants.new()
 
@@ -63,8 +34,14 @@ func _process(delta: float) -> void:
 	if(pressingE and closeToDoor):
 		_change_scene()
 
+func _physics_process(_delta: float) -> void:
+	if(!%Player):
+		print("NO PLAYER DETECTED")
+		return
+	get_tree().call_group("Enemies", "update_target_location", %Player.global_transform.origin)
+
 func _change_scene():
-	var sceneParser = "res://Rooms/" + next_scene + ".tscn"
+	var sceneParser = "res://Rooms/" + str(next_scene) + ".tscn"
 	print(sceneParser)
 	get_tree().change_scene_to_file(sceneParser)
 
---
+++ star-knight/Rooms/tutorial_room_5.gd
@@ -0,0 +1,77 @@
+extends Node3D
+
+@onready var player = %Player
+@onready var mesh = %NavigationRegion3D
+var roomConstants = RoomConstants.new()
+
+@onready var current_scene = get_tree().current_scene.name
+var next_scene
+var next_scene_index
+@onready var closeToDoor = false
+@onready var pressingE = false
+
+
+func _ready():
+	InteractEmitter.connect("CanInteract", setnear)
+	InteractEmitter.connect("CantInteract", setfar)
+	run_dialogue("TutorialRoom5")
+	print(current_scene)
+	##Tutorial Rooms will go in sequence
+	if current_scene in roomConstants.TutorialRooms:
+		next_scene_index = roomConstants.TutorialRooms.find(current_scene,0) + 1
+		if (next_scene_index >= roomConstants.TutorialRooms.size()):
+			next_scene = "HubRoom"
+		else:
+			next_scene = roomConstants.TutorialRooms[next_scene_index]
+		print(next_scene)
+	##Any other room will randomly proceed current room
+	elif  current_scene in roomConstants.Rooms:
+		next_scene_index = roomConstants.Rooms.find(current_scene,0) + randi_range(0, roomConstants.Rooms.size())
+		next_scene = roomConstants.Rooms[next_scene_index]
+
+	##Tutorial Rooms will go in sequence
+	if current_scene in roomConstants.TutorialRooms:
+		next_scene_index = roomConstants.TutorialRooms.find(current_scene,0) + 1
+		if (next_scene_index >= roomConstants.TutorialRooms.size()):
+			next_scene = "HubRoom"
+		else:
+			next_scene = roomConstants.TutorialRooms[next_scene_index]
+		print(next_scene)
+	##Any other room will randomly proceed current room
+	elif  current_scene in roomConstants.Rooms:
+		next_scene_index = roomConstants.Rooms.find(current_scene,0) + randi_range(0, roomConstants.Rooms.size())
+		next_scene = roomConstants.Rooms[next_scene_index]
+
+func run_dialogue(dialogue_string):
+	Dialogic.start(dialogue_string)
+
+#Called logic in every room so the enemies can know where the player is.
+func _physics_process(delta: float) -> void:
+	if(!player):
+		print("NO PLAYER DETECTED")
+		return
+	get_tree().call_group("Enemies", "update_target_location", player.global_transform.origin)
+
+func _change_scene():
+	var sceneParser = "res://Rooms/" + next_scene + ".tscn"
+	print(sceneParser)
+	get_tree().change_scene_to_file(sceneParser)
+
+func _on_player_pressing_interact():
+	pressingE = true
+
+func _process(delta: float) -> void:
+	if Input.is_physical_key_pressed(KEY_E):
+		pressingE = true
+	else:
+		pressingE = false
+
+	
+	if(pressingE and closeToDoor):
+		_change_scene()
+
+
+func setnear():
+	closeToDoor = true
+func setfar():
+	closeToDoor = false
