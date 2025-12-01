9477adc2e0aad72b6f24d8f4b224d56643952039
awhipple906 <alexander.cary.whipple@gmail.com>
2025-12-01 13:01:05 -0500

    Updating Hub Room to allow player to leave

--- star-knight/Rooms/hub_room.gd
+++ star-knight/Rooms/hub_room.gd
@@ -1,11 +1,59 @@
 extends Node3D
 
+var roomConstants = RoomConstants.new()
+
+@onready var current_scene = get_tree().current_scene.name
+var next_scene
+var next_scene_index
+@onready var closeToDoor = false
+@onready var pressingE = false
 
 func _ready():
 	%Player.hitbox.health_component.health = PlayerVar.playerMaxHealth
 	PlayerVar.playerCurrentHealth = PlayerVar.playerMaxHealth
 	if Dialogic.VAR.HasSeen == false:
 		run_dialogue("HubFirstConvo")
+		
+	InteractEmitter.connect("CanInteract", setnear)
+	InteractEmitter.connect("CantInteract", setfar)
+	print(current_scene)
+	##Tutorial Rooms will go in sequence
+	if current_scene in roomConstants.TutorialRooms:
+		next_scene_index = roomConstants.TutorialRooms.find(current_scene,0) + 1
+		if (next_scene_index > roomConstants.TutorialRooms.size()):
+			next_scene = "HubRoom"
+		else:
+			next_scene = roomConstants.TutorialRooms[next_scene_index]
+		print(next_scene)
+	##Any other room will randomly proceed current room
+	elif  current_scene in roomConstants.Rooms:
+		next_scene_index = randi_range(0, roomConstants.Rooms.size() -1)
+		next_scene = roomConstants.Rooms[next_scene_index]
 
 func run_dialogue(dialogue_string):
 	Dialogic.start(dialogue_string)
+
+
+func _process(delta: float) -> void:
+	if Input.is_physical_key_pressed(KEY_E):
+		pressingE = true
+	else:
+		pressingE = false
+	if(pressingE and closeToDoor):
+		_change_scene()
+
+func _physics_process(_delta: float) -> void:
+	if(!%Player):
+		print("NO PLAYER DETECTED")
+		return
+	get_tree().call_group("Enemies", "update_target_location", %Player.global_transform.origin)
+
+func _change_scene():
+	var sceneParser = "res://Rooms/" + str(next_scene) + ".tscn"
+	print(sceneParser)
+	get_tree().change_scene_to_file(sceneParser)
+
+func setnear():
+	closeToDoor = true
+func setfar():
+	closeToDoor = false
